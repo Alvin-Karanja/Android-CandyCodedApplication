4e32c5826c647907b606ae4f8dd899f6a14d19e8
Alvin-Karanja <alvin2@protonmail.com>
2020-09-25 11:29:04 +0300

    start the activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -46,5 +46,6 @@ public class InfoActivity extends AppCompatActivity {
     public void createPhoneIntent(View view) {
         Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
         Uri uri = Uri.parse("tel:0123456789");
+        phoneIntent.setData(uri);
     }
 }
